dd6a127c24b5e8f2ac2459a97381e510c14b31a2
HalaszGergo123 <halasz.gergo2001@gmail.com>
2021-02-25 10:55:16 +0200

    add Hello World project OOP style.

---
+++ 01-25 feb/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_25_feb
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            World w1 = new World(1);
+
+            w1.SayHello();
+
+            World w2 = new World(2);
+            w2.SayHello();
+        }
+    }
+}
---
+++ 01-25 feb/World.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01_25_feb
+{
+    class World
+    {
+        private int id; // orice camp de data sa fie private
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello from {id}");   
+        }
+        /// <summary>
+        /// Metoda constructor. Are acelasi nume ca si clasa
+        /// Nu are tip de return,nici macar void
+        /// Initializeaza obiectul
+        /// Se apeleaza automat la crearea/instantierea unui obiect
+        /// </summary>
+        /// <param name="id"></param>
+        public World(int id)
+        {
+            this.id = id; 
+        }
+    }
+}
